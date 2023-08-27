4094dbff85e3b722b0476443a20747a389b9ecf5
minh-p <minhisverynormal@gmail.com>
2023-08-26 21:49:44 -0700

    Added Button to stop the round and restart timer.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-// import { useState } from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import renderTime from './components/renderTime'
@@ -18,6 +18,8 @@ export default function Home() {
   const [chosenAppeal, setChosenAppeal] = useState<string>("")
   const [chosenResponseIndex, setChosenResponseIndex] = useState<string>("")
   */
+  const [roundIsActive, setRoundStatusActive] = useState<boolean>(true)
+  const [key, setKey] = useState<number>(0)
 
   return (
     <section className="p-10">
@@ -30,19 +32,33 @@ export default function Home() {
         className="absolute top-[200px] right-0 left-0 bottom-[200px] m-auto max-h-screen"
       />
 
-      {/*Circle Timer gotten from amazing package.*/}
-      <div className="absolute right-5 top-[70px]">
+      <div className="text-center absolute right-5 top-[70px]">
+        {/*Circle Timer gotten from amazing package.*/}
         <CountdownCircleTimer
           isPlaying
-          duration={ROUND_DURATION_IN_SECONDS - 1}
+          key={key}
+          duration={roundIsActive ? ROUND_DURATION_IN_SECONDS - 1 : 0}
+          initialRemainingTime={ROUND_DURATION_IN_SECONDS - 1}
           colors={['#004777', '#F7B801', '#A30000', '#A30000']}
           colorsTime={[10, 6, 3, 0]}
           size={120}
           strokeWidth={7}
-          //onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+          onComplete={() => {
+            setRoundStatusActive(false)
+          }}
         >
           {renderTime}
         </CountdownCircleTimer>
+        {/*Button to end or restart the round.*/}
+        <button
+          className="my-2 py-2.5 px-2.5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-gray border border-black border-2 hover:bg-gray-100 rounded opacity-80"
+          onClick={() => {
+            setRoundStatusActive(!roundIsActive)
+            setKey((prevKey) => prevKey + 1)
+          }}
+        >
+          {roundIsActive ? 'Stop Round' : 'Restart Round'}
+        </button>
       </div>
     </section>
   )
